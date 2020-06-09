5a6c8cc4ee901646e259efb4c6cd9cefbd90a4dd
divyanaimisha <31345605+divyanaimisha@users.noreply.github.com>
2020-06-09 17:54:55 +0530

    Updates

---
+++ precache-manifest.91c0743c794039fe5b92c8236d8e8cb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c837ddbbddcbd97772a",
+    "url": "/alumni/static/css/main.fa8a4066.chunk.css"
+  },
+  {
+    "revision": "6c837ddbbddcbd97772a",
+    "url": "/alumni/static/js/main.6c837ddb.chunk.js"
+  },
+  {
+    "revision": "887eafdebfd410c76fc9",
+    "url": "/alumni/static/js/runtime~main.887eafde.js"
+  },
+  {
+    "revision": "c8283a1db49c6dcecb8c",
+    "url": "/alumni/static/js/2.c8283a1d.chunk.js"
+  },
+  {
+    "revision": "23069abbaa03158948ca2a7bdd630936",
+    "url": "/alumni/index.html"
+  }
+];
